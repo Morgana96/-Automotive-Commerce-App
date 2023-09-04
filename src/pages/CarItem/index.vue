d2b7115cba2edc29f98a46e95413269ab6283b98
Morgana <zhuh4574@gmail.com>
2023-09-04 16:45:54 +0200

    display loading when fatching data

--- src/pages/CarItem/index.vue
+++ src/pages/CarItem/index.vue
@@ -1,7 +1,8 @@
 <template>
     <div class="bg-gray-900 min-h-screen w-full">
       <div class="flex justify-center">
-        <p class="text-white text-3xl absolute text-center mt-20 " v-show="items.length==0">Sorry, there is no result for searching</p>
+        <p v-show="loading">Loading</p>
+        <p class="text-white text-3xl absolute text-center mt-20 " v-show="items.length==0 && !loading">Sorry, there is no result for searching</p>
         <ul class="list-none grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-x-1 w-full my-14">
           <li class="bg-gray-300 p-5 pb-10 placeholder-amber-700 m-2 rounded-lg" v-for="item in items" :key ='item.id'>
             <h3 class="text-lg font-bold">{{item.make+' '+item.model}}</h3>
@@ -26,13 +27,16 @@
       const items = ref([])
       const route = useRoute()
       const keyword = ref(route.params.keyword || '')
+      const loading = ref(true)
 
       console.log(keyword);
       console.log(fetchCarsData());
 
       const fetchData = async () =>{
         try{
+          loading.value = true
           const response = await fetchCarsData()
+          loading.value = false
           items.value = response.data.filter(item => {
           return (
             item.make.toLowerCase().includes(keyword.value.toLowerCase()) ||
@@ -57,6 +61,7 @@
 
       return {
           items,
+          loading
       }
     
     }}
