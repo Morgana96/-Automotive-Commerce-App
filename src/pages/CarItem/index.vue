f687f9902f14c425cc2c6e23ecd2aba952fe9762
Morgana <zhuh4574@gmail.com>
2023-09-04 16:48:59 +0200

    style for loading

--- src/pages/CarItem/index.vue
+++ src/pages/CarItem/index.vue
@@ -1,7 +1,7 @@
 <template>
     <div class="bg-gray-900 min-h-screen w-full">
       <div class="flex justify-center">
-        <p v-show="loading">Loading</p>
+        <p class="text-white text-3xl absolute text-center mt-20 " v-show="loading">Loading</p>
         <p class="text-white text-3xl absolute text-center mt-20 " v-show="items.length==0 && !loading">Sorry, there is no result for searching</p>
         <ul class="list-none grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-x-1 w-full my-14">
           <li class="bg-gray-300 p-5 pb-10 placeholder-amber-700 m-2 rounded-lg" v-for="item in items" :key ='item.id'>
